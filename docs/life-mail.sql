4ee1d4c26db0b91ec9366b3734b6d10134304558
tao.zeng <tao.zeng@eoitek.com>
2018-11-08 22:48:08 +0800

    z

--- docs/life-mail.sql
+++ docs/life-mail.sql
@@ -11,11 +11,29 @@
  Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 28/09/2018 11:33:47
+ Date: 08/11/2018 22:47:18
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for mail_attachment
+-- ----------------------------
+DROP TABLE IF EXISTS `mail_attachment`;
+CREATE TABLE `mail_attachment` (
+  `id` bigint(20) NOT NULL,
+  `created` bigint(20) NOT NULL COMMENT '创建时间',
+  `updated` bigint(20) NOT NULL COMMENT '修改时间',
+  `created_by` bigint(20) NOT NULL COMMENT '创建人',
+  `updated_by` bigint(20) NOT NULL COMMENT '修改人',
+  `deleted` int(2) NOT NULL COMMENT '删除标识',
+  `file_name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '附件名称',
+  `file_path` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '附件地址',
+  `file_size` bigint(20) DEFAULT NULL COMMENT '附件大小',
+  `mail_id` bigint(20) NOT NULL COMMENT '邮件id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
 -- Table structure for mail_info
@@ -24,11 +42,14 @@ DROP TABLE IF EXISTS `mail_info`;
 CREATE TABLE `mail_info` (
   `id` bigint(20) NOT NULL,
   `receiver` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '接收者',
-  `mail_subject` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '邮件主题',
-  `mail_content` text COLLATE utf8mb4_bin NOT NULL COMMENT '邮件内容',
-  `file_path` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '附件地址',
-  `created` datetime DEFAULT NULL COMMENT '创建时间',
-  `updated` datetime DEFAULT NULL COMMENT '修改时间',
+  `subject` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '邮件主题',
+  `content` text COLLATE utf8mb4_bin NOT NULL COMMENT '邮件内容',
+  `created` bigint(20) NOT NULL COMMENT '创建时间',
+  `updated` bigint(20) NOT NULL COMMENT '修改时间',
+  `deleted` int(2) NOT NULL COMMENT '删除标识，0-> 未删除 1->已删除',
+  `created_by` bigint(20) NOT NULL COMMENT '创建人',
+  `updated_by` bigint(20) NOT NULL COMMENT '修改人',
+  `classify` enum('sample','html','attachment','resource') COLLATE utf8mb4_bin NOT NULL COMMENT '邮件类型，sample、html、attachments、resource',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
